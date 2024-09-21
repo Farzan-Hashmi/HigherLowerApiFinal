5e46e57a6659881de3b0adaae090be884b13d39d
Farzan Hashmi <farzan.hashmi@berkeley.edu>
2024-09-20 18:45:50 -0700

    add

--- main.py
+++ main.py
@@ -22,13 +22,14 @@ oldtime = time.time()
 async def root():
     global oldtime
     global top_videos
-    if (time.time() - oldtime > 5400):
+    if time.time() - oldtime > 5400:
         top_videos = RandomYoutubeVideos()
         oldtime = time.time()
     random_two = top_videos.sample(n=2)
     print(random_two)
-    return {"video1_id": random_two.iloc[0]['video_id']
-            , "video1_views": random_two.iloc[0]['num_views']
-            , "video2_id": random_two.iloc[1]['video_id']
-            , "video2_views": random_two.iloc[1]['num_views']}
-    
+    return {
+        "video1_id": random_two.iloc[0]["video_id"],
+        "video1_views": random_two.iloc[0]["num_views"],
+        "video2_id": random_two.iloc[1]["video_id"],
+        "video2_views": random_two.iloc[1]["num_views"],
+    }
--- utils.py
+++ utils.py
@@ -3,26 +3,27 @@ import pandas as pd
 from googleapiclient.discovery import build
 from dotenv import load_dotenv
 import os
+
 load_dotenv()
 
-YOUTUBE_API_KEY = os.getenv("YOUTUBE_API_KEY")
+YOUTUBE_API_KEY = "AIzaSyArVOc9O8i4CekkyyvyhEmIrh5PU3uC6PA"
+
 
 def RandomYoutubeVideos():
     api_key = YOUTUBE_API_KEY
     ids = []
     viewCounts = []
 
-   
     categories = [2, 1, 17, 22, 23, 24, 25, 26, 28]
 
     try:
         for number in categories:
-            youtube = build('youtube', 'v3', developerKey=api_key)
+            youtube = build("youtube", "v3", developerKey=api_key)
             request = youtube.videos().list(
-                part='statistics',
-                chart='mostPopular',
+                part="statistics",
+                chart="mostPopular",
                 maxResults=50,
-                videoCategoryId=number
+                videoCategoryId=number,
             )
 
             print("Category: " + str(number))
@@ -30,16 +31,15 @@ def RandomYoutubeVideos():
             response = request.execute()
             for i in range(0, 50):
                 print(i)
-                ids.append(response['items'][i]['id'])
-                viewCounts.append(response['items'][i]['statistics']['viewCount'])
+                ids.append(response["items"][i]["id"])
+                viewCounts.append(response["items"][i]["statistics"]["viewCount"])
 
     except Exception as e:
         print(e)
         pass
 
-    df = pd.DataFrame(list(zip(ids, viewCounts)),
-                    columns=['video_id', 'num_views'])
+    df = pd.DataFrame(list(zip(ids, viewCounts)), columns=["video_id", "num_views"])
 
-    with pd.option_context('display.max_rows', 999):
+    with pd.option_context("display.max_rows", 999):
         print(df)
-    return(df)
+    return df
