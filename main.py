1e86a11e5510abb281c578480f8fefa4f647bbf7
Farzan Hashmi <farzan.hashmi@berkeley.edu>
2024-09-20 19:05:29 -0700

    port

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+import uvicorn
 from utils import RandomYoutubeVideos
 from fastapi.middleware.cors import CORSMiddleware
 import time
@@ -34,3 +35,7 @@ async def root():
         "video2_id": random_two.iloc[1]["video_id"],
         "video2_views": random_two.iloc[1]["num_views"],
     }
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000)
